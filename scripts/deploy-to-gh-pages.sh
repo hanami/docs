4f4f7f96aef05670378234a1555e4c2895d61bd2
Anton Davydov <antondavydov.o@gmail.com>
2017-03-29 03:13:29 +0300

    Update scripts for deploy

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -o errexit
 
-rm -rf public
-mkdir public
+rm -rf docs
 
 # config
 git config --global user.email "antondavydov.o@gmail.com"
@@ -16,4 +15,4 @@ git clone -b gh-pages git@github.com:hanami/docs.git ./docs
 cd docs
 git add .
 git commit -m "Deploy to Github Pages"
-git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/hanami/docs.git" master:gh-pages > /dev/null 2>&1
