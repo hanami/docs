494056149ce03ee8884c119d392698001b5ade38
Luca Guidi <me@lucaguidi.com>
2017-10-20 09:11:09 +0200

    Add a select tag to switch between Hanami versions

--- template/fulldoc/html/assets/site.js
+++ template/fulldoc/html/assets/site.js
@@ -4,4 +4,12 @@
     $source_code = $button.parent().parent().find('.source_code')
     $source_code.toggle()
   });
+
+  var version = window.location.pathname.replace("/docs", "").replace(/\/$/, '');
+  $("#select-version option[value='"+version+"']").prop("selected", true);
+
+  $("#select-version").on("change", function(){
+    var destination = "/" + this.value;
+    window.location.pathname = destination;
+  });
 }(jQuery));
