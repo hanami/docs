a1e1f26ccd9d0895b1f9cd331ef599cec75e3357
Anton Davydov <antondavydov.o@gmail.com>
2017-10-24 12:44:46 +0300

    Add token for clone repo

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -8,11 +8,12 @@ git config --global user.email "antondavydov.o@gmail.com"
 git config --global user.name "Anton Davydov"
 
 # build
-git clone -b gh-pages git@github.com:hanami/docs.git ./docs
+git clone -b gh-pages "https://${GH_TOKEN}@github.com/hanami/docs.git" ./docs
 ./bin/build
 
 # deploy
 cd docs
 git add .
 git commit -m "Deploy to Github Pages"
-git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/hanami/docs.git" master:gh-pages > /dev/null 2>&1
+git remote add origin-pages https://${GH_TOKEN}@github.com/hanami/docs.git
+git push --quiet origin-pages gh-pages 
